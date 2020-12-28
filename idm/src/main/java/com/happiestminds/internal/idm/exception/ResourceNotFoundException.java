8908e8a285919124acd286a0a9ed766e2d41b94c
Rupesh <rupesh.kumar@happiestminds.com>
2020-12-28 22:57:06 +0530

    exception handling improvements

--- idm/src/main/java/com/happiestminds/internal/idm/exception/ResourceNotFoundException.java
+++ idm/src/main/java/com/happiestminds/internal/idm/exception/ResourceNotFoundException.java
@@ -4,17 +4,17 @@ import com.happiestminds.internal.idm.web.response.ErrorMessage;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-    private ErrorMessage errorMessage;
+  private ErrorMessage errorMessage;
 
-    public ResourceNotFoundException(ErrorMessage errorMessage) {
-        this.errorMessage = errorMessage;
-    }
+  public ResourceNotFoundException(ErrorMessage errorMessage) {
+    this.errorMessage = errorMessage;
+  }
 
-    public ErrorMessage getErrorMessage() {
-        return errorMessage;
-    }
+  public ErrorMessage getErrorMessage() {
+    return errorMessage;
+  }
 
-    public void setErrorMessage(ErrorMessage errorMessage) {
-        this.errorMessage = errorMessage;
-    }
+  public void setErrorMessage(ErrorMessage errorMessage) {
+    this.errorMessage = errorMessage;
+  }
 }
